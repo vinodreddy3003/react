ab3ae2c06235accad437fb4feaaae88ced0d8e7e
vinod <vinod20010330@gmail.com>
2024-08-25 22:11:34 +0530

    message

--- src/app.js
+++ src/app.js
@@ -1,5 +1,41 @@
-const numbers = [2,4,6,8]
-const great=numbers.reduce((x,y)=>{
-    return x=x+y
-})
-console.log(great)
+function Getname(name){
+    return name;
+}
+a=false;
+b=true;
+console.log(a||Getname("vinod"));
+//literals
+let c="vinod";
+let d="reddy";
+console.log(`${c} ${d}` );
+
+const array =[1,2,3];
+const[firstElement,SecondElement]=array;
+console.log(firstElement,SecondElement)
+
+const personName=[
+    {
+        names:"vinod",
+        age:17,
+        place:"usa",
+    },
+    {
+        names:"person2",
+        age:18,
+        place:"usas",
+    },
+    {
+        names:"person3",
+        age:19,
+        place:"usass",
+    }
+]
+const getAllNames=personName.map((singlePerson,index)=>{
+    console.log(singlePerson,index);
+    return singlePerson.age
+    
+    
+});
+console.log(getAllNames);
+
+
